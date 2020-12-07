0777979e28a5eb5c95b7afbac9a1720ec8402887
kotaro.susa <susanoo0428@gmail.com>
2020-12-07 07:44:00 +0000

    kadai

---
+++ app/controllers/tasklist2s_controller.rb
@@ -0,0 +1,55 @@
+class Tasklist2sController < ApplicationController
+  def index
+    @tasklist2s = Tasklist2.all
+  end
+
+  def show
+    @tasklist2 = Tasklist2.find(params[:id])
+  end
+  
+  def new
+    @tasklist2 = Tasklist2.new
+  end
+
+  def create
+    @tasklist2 = Tasklist2.new(tasklist2_params)
+
+    if @tasklist2.save
+      flash[:success] ="タスクが正常に投稿されました"
+      redirect_to @tasklist2
+    else
+      flash.now[:danger] = 'タスク が投稿されませんでした'
+      render:new
+    end
+  end
+
+  def edit 
+   @tasklist2 = Tasklist2.find(params[:id])
+  end
+
+  def update
+     @tasklist2 =Tasklist2.find(params[:id])
+
+    if @tasklist2.update(tasklist2_params)
+      flash[:success] = 'タスクは正常に更新されました'
+      redirect_to tasklist2_url(@tasklist2)
+    else
+      flash.now[:danger] = 'タスクは更新されませんでした'
+      render :edit
+    end
+  end
+
+  def destroy
+    @tasklist2 = Tasklist2.find(params[:id])
+    @tasklist2.destroy
+    
+    flash[:success] = "タスクは正常に削除されました"
+    redirect_to tasklist2s_url
+  end
+  
+  private
+  
+  def tasklist2_params
+    params.require(:tasklist2).permit(:content)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "tasklist2s#index"
+  
+  resources :tasklist2s
 end
